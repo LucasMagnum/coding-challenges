0684f58d1b26313fc6b25ed93e862399f7572358
Lucas Magnum <lucasmagnum2@gmail.com>
2020-01-07 10:19:02 -0300

    Add DP solutions for questions 8.5 and 8.6

---
+++ python/dynamic_programming/question_8_5.py
@@ -0,0 +1,70 @@
+"""
+    Recursive Multiply:
+        Write a recursive function to multiply two positive integers without
+        using the * operator. You can use addition, substraction, and bit shifting, but
+        you should minimize the number of those operations.
+"""
+
+def brute_force(a, b):
+    if a == 0 or b == 0:
+        return 0
+
+    if b == 1:
+        return a
+
+    return a + brute_force(a, b - 1)
+
+
+def doubling_sum(a, b):
+    smaller, bigger = (a, b) if a < b else (b, a)
+
+    def _multiply(smaller, bigger):
+        if smaller == 0: return 0
+        if smaller == 1: return bigger
+
+        smaller_divided = smaller >> 1
+        left_side = _multiply(smaller_divided, bigger)
+        right_side = left_side
+
+        if smaller % 2 == 1:
+            right_side = _multiply(smaller - smaller_divided, bigger)
+
+        return left_side + right_side
+
+    return _multiply(smaller, bigger)
+
+def doubling_sum_cached(a, b):
+    smaller, bigger = (a, b) if a < b else (b, a)
+
+    def _multiply(smaller, bigger, cache=None):
+        if cache is None:
+            cache = {}
+
+        if smaller == 0: return 0
+        if smaller == 1: return bigger
+
+        if cache.get(smaller):
+            return cache[smaller]
+
+        smaller_divided = smaller >> 1
+        left_side = _multiply(smaller_divided, bigger, cache)
+        right_side = left_side
+
+        if smaller % 2 == 1:
+            right_side = _multiply(smaller - smaller_divided, bigger, cache)
+
+        sides_sum = left_side + right_side
+        cache[smaller] = sides_sum
+        return sides_sum
+
+    return _multiply(smaller, bigger)
+
+if __name__ == "__main__":
+    a, b = (1604, 800)  # 800 iterations
+    print("Solution: ", brute_force(a, b))
+
+    a, b = (1604, 800)  # 26 iterations
+    print("Solution doubling sum: ", doubling_sum(a, b))
+
+    a, b = (1604, 800)  # 18 iterations
+    print("Solution doubling sum cached: ", doubling_sum_cached(a, b))
---
+++ python/dynamic_programming/question_8_6.py
@@ -0,0 +1,52 @@
+"""
+Towers of Hanoi:
+    In the classic problem of the Towers of Hanoi, you have 3 towers and N disks of different
+    sizes which can slide onto another. The puzzle starts with the disks sorted in ascending
+    order of size from top to bottom. You have the following constraints:
+
+        * Only one disk can be moved at a time
+        * A disk is slid off the top of one tower onto another tower
+        * A disk cannot be placed on top of a smaller disk.    
+
+"""
+import sys
+
+
+class Tower:
+    def __init__(self, disks=None):
+        if not disks:
+            disks = []
+        self.disks = disks
+
+    def add(self, disk):
+        if self.disks and self.disks[-1] <= disk:
+            raise ValueError(f"{disk} <= {self.disks[-1]}")
+        self.disks.append(disk)
+
+    def move_to_top(self, tower):
+        top = self.disks.pop()
+        tower.add(top)
+    
+    def move_disks(self, quantity, destination, buffer):
+        if quantity <= 0: 
+            return
+
+        self.move_disks(quantity - 1, buffer, destination)
+        self.move_to_top(destination)
+        buffer.move_disks(quantity - 1, destination, self)
+
+if __name__ == "__main__":
+    try:
+        N = int(sys.argv[1])
+    except IndexError:
+        N = 3
+
+    tower_01 = Tower([x for x in range(N, 0, -1)])
+    tower_02 = Tower()
+    tower_03 = Tower()
+
+    print("Towers in: ", [tower_01.disks, tower_02.disks, tower_03.disks])
+    tower_01.move_disks(len(tower_01.disks), tower_03, tower_02)
+    print("Towers out: ", [tower_01.disks, tower_02.disks, tower_03.disks])
+
+
