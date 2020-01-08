6add0083f8e9b4ed83bb51eee99ab469feca7641
Lucas Magnum <lucasmagnum2@gmail.com>
2020-01-07 21:53:56 -0300

    Add  solution for DP question 8.7

---
+++ python/dynamic_programming/question_8_7.py
@@ -0,0 +1,28 @@
+"""
+    Permutations without Dups:
+        Write a method to compute all permutations of a string 
+        of unique characters
+"""
+
+
+def recursive(string):
+    if len(string) == 0:
+        return [""]
+    
+    first_char = string[0]
+    reminder = string[1:]
+
+    permutations = recursive(reminder) # [b]
+
+    new_permutations = []
+    for permutation in permutations:
+        for index in range(len(permutation) + 1):
+            new_permutations.append(permutation[:index] + first_char + permutation[index:])
+    return new_permutations
+    
+
+if __name__ == "__main__":
+    string = "abcde"
+    print("Solution: ", recursive(string))
+
+    import itertools
