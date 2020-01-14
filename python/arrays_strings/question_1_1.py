846ffae5d87effc1ee895f9e897a1490aba5aecf
Lucas Magnum <lucasmagnum2@gmail.com>
2020-01-14 16:22:56 -0300

    Add solutions for questions 1.1 and 1.2 in Array and Strings.

---
+++ python/arrays_strings/question_1_1.py
@@ -0,0 +1,54 @@
+"""
+    Is Unique:
+         Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?
+"""
+import sys
+
+
+def brute_force_solution(string):
+    """
+    Check each character with all the remaining ones.
+    """
+    for index, character in enumerate(string):
+        for another_character in string[index + 1:]:
+            if character == another_character:
+                return False
+    return True
+
+
+def count_solution(string):
+    """Keep tracks of the seen characters."""
+    counter = {}
+
+    for character in string:
+        if character in counter:
+            return False
+        counter[character] = True
+
+    return True
+
+
+def bit_array_solution(string):
+    """
+    Create a bit array and uses it to toggle a character based on its number.
+    """
+    checker = 0
+
+    for character in string:
+        value = ord(character) - ord("a")
+        if checker & (1 << value) > 0:
+            return False
+        checker |= (1 << value)
+    return True
+
+
+if __name__ == "__main__":
+    try:
+        string = sys.argv[1]
+    except IndexError:
+        string = "Lucas"
+
+    print("Brute force: ", brute_force_solution(string))
+    print("Counting: ", count_solution(string))
+    print("Bit Array: ", bit_array_solution(string))
---
+++ python/arrays_strings/question_1_2.py
@@ -0,0 +1,71 @@
+"""
+    Check permutation:
+        Given two strings, write a method to decide if one is a permutation of the
+        other.
+"""
+import itertools
+import sys
+
+
+def brute_force(first: str, second: str) -> bool:
+    """Brute force solution checking if there's a permutation 
+    equal to the value."""
+    if len(first) != len(second):
+        return False
+
+    permutations = itertools.permutations(first)
+    second = tuple(second)
+
+    for permutation in permutations:
+        if permutation == second:
+            return True
+
+    return False
+
+
+def solution(first: str, second: str) -> bool:
+    """Counting the characters solution.
+    A permutation will have the same amount of characters."""
+    if len(first) != len(second):
+        return False
+
+    counter = {}
+    
+    for character in first:
+        counter[character] = counter.get(character, 0) + 1
+    
+    for character in second: 
+        count = counter.get(character, 0)
+        if count - 1 < 0:
+            return False
+        counter[character] = count - 1 
+
+    for k, v in counter.items():
+        if v != 0:
+            return False
+
+    return True
+
+def sorting_solution(first: str, second: str) -> True:
+    """Sort both strings and check each character."""
+    if len(first) != len(second):
+        return False
+
+    first = sorted(first)
+    second = sorted(first)
+
+    for index in range(len(first)):
+        if first[index] != second[index]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    try:
+        first, second = sys.argv[1:]
+    except ValueError:
+        first, second = ("god", "dog")
+    
+    print(f"Solution: ({first}, {second}): ", solution(first, second))
+    print(f"Solution (sorting): ({first}, {second}): ", solution(first, second))
+    print(f"Brute force: ({first}, {second}): ", brute_force(first, second))
