a68c0f98a9ad004c8e9435c72c0ef80e6b6b3afc
Lucas Magnum <lucasmagnum2@gmail.com>
2020-01-21 10:09:54 -0300

    Add answers for questions 3.3 and 3.4 (Stacks/Queues) with Python

---
+++ python/stacks_queues/question_3_3_.py
@@ -0,0 +1,43 @@
+"""
+    Stack of Plates: 
+    
+    Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+    Therefore, in real life, we would likely start a new stack when the previous stack exceeds some
+    threshold. Implement a data structure SetOfStack s that mimics this. SetOfStack s should be
+    composed of several stacks and should create a new stack once the previous one exceeds capacity,
+    SetOfStacks . push() and SetOfStacks.pop O should behave identically to a single stack
+    (that is, pop() should return the same values as it would if there were just a single stack).
+    FOLLOW UP
+    Implement a function popAt (in t index ) which performs a pop operation on a specific substack
+"""
+
+class SetOfStacks:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.current_stack = []
+        self.stacks = [self.current_stack]
+
+    def push(self, item):
+        if len(self.current_stack) == self.max_size:
+            self.current_stack = []
+            self.stacks.append(self.current_stack)
+
+        self.current_stack.append(item)
+
+    def pop(self, stack_index=None):
+        if stack_index is not None:
+            return self.stacks[stack_index].pop()
+        return self.current_stack.pop()
+
+    def __len__(self):
+        return sum([len(stack) for stack in self.stacks])
+
+if __name__ == "__main__":
+    stack = SetOfStacks(max_size=25)
+
+    for x in range(1, 101):
+        stack.push(x)
+
+    assert len(stack) == 100
+    assert stack.pop() == 100
+    assert stack.pop(0) == 25
---
+++ python/stacks_queues/question_3_4.py
@@ -0,0 +1,46 @@
+"""
+Queue via Stacks: 
+    Implement a MyQueue class which implements a queue using two stacks. 
+"""
+
+class Stack:
+    def __init__(self):
+        self._data = []
+    
+    def push(self, item):
+        self._data.append(item)
+    
+    def pop(self):
+        return self._data.pop()
+
+    def __len__(self):
+        return len(self._data)
+
+
+class Queue:
+    def __init__(self):
+        self.stack_01 = Stack()
+        self.stack_02 = Stack()
+    
+    def enqueue(self, item):
+        self.stack_01.push(item)
+
+    def dequeue(self):
+        if not self.stack_02:
+            while self.stack_01:
+                self.stack_02.push(self.stack_01.pop())
+
+        return self.stack_02.pop()
+
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.enqueue(5)
+
+    assert queue.dequeue() == 1
+    assert queue.dequeue() == 2
+    assert queue.dequeue() == 3
